533494fb5a0997910ca87364349e6aa40f61e981
MasterDurian <wzha0172@student.monash.edu>
2024-08-05 13:51:58 +1000

    Add .gitattributes file to handle line endings

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "melbournecycling": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "melbournecyclingc49f0737": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3ed575105": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
